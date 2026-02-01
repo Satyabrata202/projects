c1327bf5ecf6b0c77794228cc134ec51f75d28cd
Satyabrata202 <98125319+Satyabrata202@users.noreply.github.com>
2026-02-01 21:10:01 +0530

    Quiz result displayed

--- Tenth/ten.js
+++ Tenth/ten.js
@@ -1,5 +1,21 @@
-const form = document.querySelector(".quizForm");
-
+const form = document.querySelector(".quiz-form");
+console.log("JS Loaded")
 form.addEventListener('submit',(e)=>{
-    
+    e.preventDefault();
+
+    const data = new FormData(form);
+    let finalScore = 0;
+    const correctAnswer = {
+      q1: "noneoftheabove",
+      q2: "script",
+      q3: "functionmyFunction",
+      q4: "add",
+      q5: "javascriptObjectNotation"
+    };
+    for (const [name,value] of data.entries()) {
+        if(correctAnswer[name] == value){
+            finalScore++;
+        }
+    }
+    document.getElementById('scoreMessage').textContent = `Your quiz score is ${finalScore}`;
 })
